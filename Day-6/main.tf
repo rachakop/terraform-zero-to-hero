9be9280eaf800caf5a8c477f820d8843e98207e5
rachakop <102464007+rachakop@users.noreply.github.com>
2025-12-24 15:36:09 +0530

    Update main.tf

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -22,3 +22,9 @@ module "ec2_instance" {
   ami = var.ami
   instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
 }
+
+lookup(...): This function searches the map:
+It looks at the instance_type map.
+It uses the current workspace name as the key.
+"t2.micro": This is the default value. If you are in a workspace that isn't in the map (like experimental), 
+it will fall back to a cheap t2.micro.
